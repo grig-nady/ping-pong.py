c37a192757847a8990fd25dc11ef742a0013efd5
grig-nady <146548857+grig-nady@users.noreply.github.com>
2023-10-07 15:54:23 +0300

    Update ping.py
    v2.0

--- ping.py
+++ ping.py
@@ -31,19 +31,42 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 
+speed_x = 3
+speed_y = 3
 background = transform.scale(image.load("Wednesday.jpg"), (win_width, win_height))
 player1 = Player('racets.png', 0, 100, 5, 75, 90)
-player2 = Player('racets.png', 500, 100, 5, 75, 90)   
+player2 = Player('racets.png', 625, 100, 5, 75, 90)   
 ball = GameSprite('boll.png', 250, 250, 5, 50, 50)      
 clock = time.Clock()
 FPS = 60
 run = True
 
+finish = False
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render('1 ПРОИГРАЛ', True, (180, 0, 0))
+lose2 = font1.render('2 ПРОИГРАЛ', True, (180, 0, 0))
+
+
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-    window.blit(background,(0,0))
+    
+    window.blit(background,(0,0)) 
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if sprite.collide_rect(player1, ball) or sprite.collide_rect(player2, ball):
+        speed_x *= -1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200,200))
+    if ball.rect.x > win_width-50 :
+        finish = True
+        window.blit(lose2, (200,200))
+    if ball.rect.y<0 or ball.rect.y>win_height-50:
+        speed_y = speed_y* (-1)
     player1.update_L()
     player2.update_R()
     player1.reset()
@@ -51,3 +74,4 @@ while run:
     ball.reset()
     display.update()
     clock.tick(FPS)
+    
